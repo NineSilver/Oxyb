616528a7172b0f5932f94cc54d05995b5f043feb
NineSilver <60185955+NineSilver@users.noreply.github.com>
2021-12-29 12:57:04 +0100

    Rewritten stivale2_get_tag to use MEM_PHYS_OFFSET

--- kernel/boot/boot.c
+++ kernel/boot/boot.c
@@ -1,18 +1,17 @@
 #include <stddef.h>
 
+#include "../mem/mm.h"
 #include "boot.h"
 
 void* stivale2_get_tag(struct stivale2_struct* info, uint64_t id)
 {
-    struct stivale2_tag* tag = (void*)info->tags;
-    for(;;)
+    struct stivale2_tag* tag = (struct stivale2_tag*)(info->tags + MEM_PHYS_OFFSET);
+    while(tag)
     {
-        if(!tag)
-            return NULL;
-
         if(tag->identifier == id)
             return tag;
             
-        tag = (void*)tag->next;
+        tag = (struct stivale2_tag*)(tag->next + MEM_PHYS_OFFSET);
     }
+    return NULL;
 }
