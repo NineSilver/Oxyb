560fda6be032024960e7f519a3edd392696c40cb
NineSilver <60185955+NineSilver@users.noreply.github.com>
2021-12-29 13:02:39 +0100

    stivale2 header now goes into boot.c

--- kernel/boot/boot.c
+++ kernel/boot/boot.c
@@ -3,6 +3,25 @@
 #include "../mem/mm.h"
 #include "boot.h"
 
+static uint8_t stack[4096];
+
+static struct stivale2_header_tag_framebuffer framebuffer_request = {
+    .tag = {
+        .identifier = STIVALE2_HEADER_TAG_FRAMEBUFFER_ID,
+        .next = 0
+    },
+    
+    .framebuffer_width  = 0,
+    .framebuffer_height = 0
+};
+
+__attribute__((section(".stivale2hdr"), used)) struct stivale2_header header2 = {
+	.entry_point = 0,
+    .stack = (uint64_t)stack + sizeof(stack),
+    .flags = 0,
+    .tags = (uint64_t)&framebuffer_request
+};
+
 void* stivale2_get_tag(struct stivale2_struct* info, uint64_t id)
 {
     struct stivale2_tag* tag = (struct stivale2_tag*)(info->tags + MEM_PHYS_OFFSET);
