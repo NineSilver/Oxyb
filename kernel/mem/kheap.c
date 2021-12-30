617e06133cc39d37e1ffc8c0139371c500657051
NineSilver <60185955+NineSilver@users.noreply.github.com>
2021-12-30 17:42:01 +0100

    Added kheap

---
+++ kernel/mem/kheap.c
@@ -0,0 +1,60 @@
+#include <string.h>
+
+#include "mm.h"
+
+struct slab* slabs = NULL;
+
+void* kalloc(size_t size)
+{
+    size_t pages = size / PAGE_SIZE;
+
+    if(size % PAGE_SIZE) pages++;
+
+    void* ptr = pmm_calloc(pages + 1);
+    if(!ptr) return NULL;
+
+    ptr += MEM_PHYS_OFFSET;
+    struct slab* this = ptr;
+    ptr += PAGE_SIZE;
+
+    this->pages = pages;
+    this->size = size;
+
+    return ptr;
+}
+
+void* krealloc(void* ptr, size_t size)
+{
+    if(!ptr) return kalloc(size);
+    if(!ptr && !size) return NULL;
+    if(!size)
+    {
+        kfree(ptr);
+        return (void*)0;
+    }
+
+    struct slab* this = (struct slab*)((size_t)ptr - PAGE_SIZE);
+    if((this->size + PAGE_SIZE - 1) / PAGE_SIZE
+         == (size + PAGE_SIZE - 1) / PAGE_SIZE)
+    {
+        this->size = size;
+        return ptr;
+    }
+
+    void* new = kalloc(size);
+    if(!new) return NULL;
+    
+    if(this->size > size)
+        memcpy(new, ptr, size);
+    else
+        memcpy(new, ptr, this->size);
+
+    kfree(ptr);
+    return new;
+}
+
+void kfree(void* ptr)
+{
+    struct slab* this = (struct slab*)((size_t)ptr - PAGE_SIZE);
+    pmm_free((void*)((size_t)this - MEM_PHYS_OFFSET), this->pages + 1);
+}
--- kernel/mem/mm.h
+++ kernel/mem/mm.h
@@ -9,6 +9,9 @@
 #define MEM_PHYS_OFFSET ((uint64_t)0xffff800000000000)
 #define MEM_KERN_OFFSET ((uint64_t)0xffffffff80000000)
 
+
+/* PMM */
+
 void init_pmm(struct stivale2_struct_tag_memmap* memmap);
 
 void* pmm_malloc(size_t pages);
@@ -16,6 +19,8 @@ void* pmm_calloc(size_t pages);
 void* pmm_realloc(void* addr, size_t new_pages, size_t old_pages);
 void pmm_free(void* addr, size_t pages);
 
+
+/* VMM */
 
 struct vmm_pagemap
 {
@@ -27,5 +32,18 @@ void vmm_load_pagemap(struct vmm_pagemap* pagemap);
 struct vmm_pagemap vmm_new_pagemap();
 void vmm_map_page(struct vmm_pagemap* pagemap, uint64_t physical, uint64_t virtual, uint64_t flags);
 void vmm_unmap_page(struct vmm_pagemap* pagemap, uint64_t virtual);
+
+
+/* Kernel heap */
+
+struct slab
+{
+    size_t pages;
+    size_t size;
+};
+
+void* kalloc(size_t size);
+void* krealloc(void* ptr, size_t size);
+void kfree(void* ptr);
 
 #endif /* !__OXYB__MEM__PMM_H */
